29660191686abcb19a908862d49c1808260c93a7
Ebi-Joe <esukujosiah2006@gmail.com>
2025-02-05 22:09:39 +0100

    Hopefully for Bookings Frontend

--- src/components/pages/ThankYou.jsx
+++ src/components/pages/ThankYou.jsx
@@ -16,11 +16,12 @@ function ThankYou() {
   }
 
   useEffect(() => {
-    if( transaction_id && tx_ref){
-      setIsProcessed(true);
-      createBooking(tx_ref, transaction_id)
+    // Only make the request if the transaction_id and tx_ref exist and the booking has not been processed
+    if (transaction_id && tx_ref && !isProcessed) {
+      createBooking(tx_ref, transaction_id);
+      setIsProcessed(true);  // Mark as processed to prevent the effect from running again
     }
-  }, [transaction_id, tx_ref, createBooking, isProcessed])
+  }, [transaction_id, tx_ref, createBooking, isProcessed]);
 
   return (
     <>
@@ -36,24 +37,14 @@ function ThankYou() {
                 <p className="pb-5">Your payment was processed <span className='text-[#02BF74]'>successfully</span></p>
 
                 <Link to="/">
-                    <button type="butto" className="bg-white text-center w-52 rounded-2xl h-14 relative font-sans text-black text-xl font-semibold group">
-                    <div className="bg-green-400 rounded-xl h-12 w-12 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
-                        <svg
-                        width="25px"
-                        height="25px"
-                        viewBox="0 0 1024 1024"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            fill="#000000"
-                            d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
-                        ></path>
-                        <path
-                            fill="#000000"
-                            d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
-                        ></path>
-                        </svg>
-                    </div>
-                    <p class="translate-x-2">Go To Home</p>
+                    <button type="button" className="bg-white text-center w-52 rounded-2xl h-14 relative font-sans text-black text-xl font-semibold group">
+                        <div className="bg-green-400 rounded-xl h-12 w-12 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+                            <svg width="25px" height="25px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+                                <path fill="#000000" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path>
+                                <path fill="#000000" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"></path>
+                            </svg>
+                        </div>
+                        <p className="translate-x-2">Go To Home</p>
                     </button>
                 </Link>
 
