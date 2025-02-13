3257f42d805d9465add9cc457efff2d75b24d02d
Ebi-Joe <esukujosiah2006@gmail.com>
2025-02-13 10:46:12 +0100

    Reviews Issue

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -26,27 +26,23 @@ function Review() {
       <div className="img flex relative justify-center items-center">
         <img src="/img/Review.jpg" alt="Review Background" className="w-screen object-cover h-[55vh] md:h-full" />
         
-        <div className="txt absolute text-white z-10 text-sm md:text-xl text-center max-w-3xl mx-auto">
-          {displayedReviews.length > 0 ? (
+        <div className="txt absolute text-white z-10 text-sm md:text-xl text-center max-w-3xl mx-auto flex flex-col items-center">
+          {displayedReviews.length > 0 && (
             <>
-              <h1 className="py-2 px-4">{displayedReviews[currentReviewIndex]?.description}</h1>
-              <h2 className="pt-2 px-4">{displayedReviews[currentReviewIndex]?.name}</h2>
+              <h1 className="py-2 px-4">{displayedReviews[currentReviewIndex].description}</h1>
+              <h2 className="pt-2 px-4">{displayedReviews[currentReviewIndex].name}</h2>
+
+              <div className="icn flex justify-center mt-4">
+                {displayedReviews.map((_, index) => (
+                  <button key={index} className={`mx-2 text-lg md:text-3xl ${currentReviewIndex === index ? 'text-[#6dc234]' : 'text-gray-400'}`}
+                    onClick={() => handleReviewChange(index)} >
+                    <i className="fa-solid fa-minus"></i>
+                  </button>
+                ))}
+              </div>
             </>
-          ) : (
-            <h1 className="py-2 px-4">No reviews available</h1>
           )}
         </div>
-
-        {displayedReviews.length > 0 && (
-          <div className="icn flex justify-center mt-4">
-            {displayedReviews.map((_, index) => (
-              <button key={index} className={`mx-2 text-lg md:text-3xl ${currentReviewIndex === index ? 'text-[#6dc234]' : 'text-gray-400'}`}
-                onClick={() => handleReviewChange(index)} >
-                <i className="fa-solid fa-minus"></i>
-              </button>
-            ))}
-          </div>
-        )}
       </div>
     </div>
   );
